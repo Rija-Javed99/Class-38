04306923745757ff5d6cc68659be6b8307320414
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-04-24 03:42:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,11 +8,11 @@ var database;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
+//create car sprites
 
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+//set canvas for screen size
   database = firebase.database();
   game = new Game();
   game.getState();
